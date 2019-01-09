2fc2a4f32f86bcf74a8fccf9b4fd2bd629b92a4e
wei shung chung <bigdataexplorer@users.noreply.github.com>
2019-01-09 00:40:32 -0800

    Update python docs

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -125,7 +125,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+#html_theme = "sphinx_rtd_theme"
+html_theme = "bizstyle"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -346,6 +348,3 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-#html_theme = "sphinx_rtd_theme"
-html_theme = "bizstyle"
--- python/docs/source/index.rst
+++ python/docs/source/index.rst
@@ -5,6 +5,11 @@
 
 Welcome to ML Featurizer's Documentation !
 ==========================================
+ML Featurizer is a library
+to enable users to create additional features from raw data with ease. 
+It extends and enriches the existing `Spark's feature engineering functionality`_ 
+
+.. _Spark's feature engineering functionality: https://spark.apache.org/docs/latest/ml-features.html
 
 Contents:
 
